f4ab0a1ca771dd4bf1ed03bc083e9a31f253d5fe
Twana Khudhur <twana.brno@gmail.com>
2024-08-07 10:22:47 +0200

    Finish Fetch 01

---
+++ fetch-ex/01/script.js
@@ -0,0 +1,28 @@
+
+      async function fetchTodos() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+          const todos = await response.json();
+          populateTodos(todos);
+        } catch (error) {
+          console.error('Error fetching todos:', error);
+        }
+      }
+
+      function populateTodos(todos) {
+        const todoList = document.getElementById('todo-list');
+        todos.forEach(todo => {
+          const todoItem = document.createElement('li');
+          todoItem.textContent = todo.title;
+          
+          if (todo.completed) {
+            todoItem.classList.add('line-through', 'text-gray-500');
+          } else {
+            todoItem.classList.add('text-gray-800');
+          }
+
+          todoList.appendChild(todoItem);
+        });
+      }
+
+      window.addEventListener('DOMContentLoaded', fetchTodos);
